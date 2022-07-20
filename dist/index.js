12fd52ef1cef0f1fafb8d46cdc1ddacf03f8688d
Tksi <Tksi@users.noreply.github.com>
2022-07-20 18:45:55 +0900

    [fix] build ESNext

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,9 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-const console_1 = require("console");
-const stackTraceParser = __importStar(require("stacktrace-parser"));
-const console = new console_1.Console({ stdout: process.stdout, stderr: process.stderr });
-const log = (...args) => {
+import { Console } from 'console';
+import * as stackTraceParser from 'stacktrace-parser';
+const console = new Console({ stdout: process.stdout, stderr: process.stderr });
+export const log = (...args) => {
     const latestTrace = stackTraceParser.parse(new Error().stack ?? '')[1];
     console.log(`\u001b[36m${latestTrace.methodName}\u001b[0m`, `${latestTrace.file?.replace('file://', '')}:${latestTrace.lineNumber}:${latestTrace.column}`);
     console.log(...args);
 };
-exports.log = log;
-exports.default = exports.log;
+export default log;
